6355006dc987a34b65d3b4e23456285055f8d6ed
Mattias Alexander Coelho <mattic15@student.uia.no>
2019-04-29 23:31:09 +0200

    fixed beackend crash

--- betauia/Models/EventModel.cs
+++ betauia/Models/EventModel.cs
@@ -5,20 +5,29 @@ namespace betauia.Models
 {
     public class EventModel
     {
-        public EventModel(string title, string subTitle, string description, string content, DateTime eventTime)
+        public EventModel(string title, string subTitle, string description, string content, DateTime eventTime, /*ApplicationUser author, */string image, int atendees, bool isPublic)
         {
             Title = title;
             SubTitle = subTitle;
             Description = description;
             Content = content;
             EventTime = eventTime;
+            //Author = author;
+            Image = image;
+            Atendees = atendees;
+            IsPublic = isPublic;
         }
         
+      
         public int Id { get; set; }
         public string Title { get; set; }
         public string SubTitle { get; set; }
         public string Description { get; set; }
         public string Content { get; set; }
+        public string Image { get; set; }
+        public int Atendees { get; set; }
+        public bool IsPublic { get; set; }
+        //private ApplicationUser Author { get; set; }
         public DateTime EventTime { get; set; }
     }
 }
