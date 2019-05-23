133fa8a2c3c57a5820819d04e820e9f688794cee
Bendik Dyrli <bendid13@uia.no>
2019-05-23 13:47:09 +0200

    edited server to localhost

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
   devServer: {
     proxy: {
       '^/api': {
-        target: 'https://192.168.1.101:5001',
+        target: 'https://localhost:5001',
         ws: true,
         changeOrigin: true,
       },
