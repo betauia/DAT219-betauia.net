1fbbddb417cbdbe1ba915fc3ea7c0ec4e4cc1e82
Mattias Alexander Coelho <mattic15@student.uia.no>
2019-08-08 16:38:40 +0100

    added RestSharp for ease of use between vipps and backend

--- betauia/Controllers/VippsCommands.cs
+++ betauia/Controllers/VippsCommands.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using betauia.Models;
+using RestSharp;
 
 namespace betauia.Controllers {
     [ApiController]
@@ -9,7 +10,30 @@ namespace betauia.Controllers {
         private readonly InitiatePaymentCommand _ipc;
         
         public VippsApiController(AuthorizationTokenResponse atr, InitiatePaymentCommand ipc) {
+            _atr = atr;
             
         }
+        public IActionResult GetVippsToken() {
+            var client = new RestClient("https://api.vipps.no/accessToken/get");
+            var request = new RestRequest(Method.POST);
+            request.AddHeader("cache-control", "no-cache");
+            request.AddHeader("Connection", "keep-alive");
+            request.AddHeader("Content-Length", "");
+            request.AddHeader("Accept-Encoding", "gzip, deflate");
+            request.AddHeader("Cookie", "x-ms-gateway-slice=prod; stsservicecookie=ests; fpc=AiuvQ7gu0vpErBq_sxNUZGiQPThUAQAAANhx3tQOAAAA");
+            request.AddHeader("Host", "api.vipps.no");
+            request.AddHeader("Postman-Token", "83621bc5-2f26-4e92-9254-1859b0929e88,394776b6-cab5-4801-bd91-d83c571c2e48");
+            request.AddHeader("Cache-Control", "no-cache");
+            request.AddHeader("Accept", "*/*");
+            request.AddHeader("User-Agent", "PostmanRuntime/7.15.2");
+            request.AddHeader("Ocp-Apim-Subscription-Key", "e3c20a6d93f94353a0fe3c0541c1eb0e");
+            request.AddHeader("client_secret", "dkJlVDhIOVdpZkFpVGs3T013aDM=");
+            request.AddHeader("client_id", "451a3d2c-d526-4cbe-b59c-940be028ba7a");
+            IRestResponse response = client.Execute(request);
+
+            
+            return Ok();
+        }
+
     }
 }
