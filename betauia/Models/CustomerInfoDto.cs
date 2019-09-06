8524acce9cafbf020c34f4e2ea6cfdf4886b9e4e
Erik Aspen <erikaa17@uia.no>
2019-09-06 21:57:07 +0200

    master merged

--- betauia/Models/CustomerInfoDto.cs
+++ betauia/Models/CustomerInfoDto.cs
@@ -1,11 +1,7 @@
 namespace betauia.Models{
 
     public class CustomerInfoDto {
-<<<<<<< HEAD
-        public string mobileNumber {get; set; }
-=======
         public string mobileNumber { get; set; }
->>>>>>> origin/master
     }
 
 }
