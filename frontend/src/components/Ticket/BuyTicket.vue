3695e57b7ca68a653f5ba73599fd6ee6c75e98d4
Mattias Alexander Coelho <mattic15@student.uia.no>
2019-10-05 21:11:57 +0200

    BETA101 prep

--- frontend/src/components/Ticket/BuyTicket.vue
+++ frontend/src/components/Ticket/BuyTicket.vue
@@ -7,10 +7,9 @@
                 </h1>
             </div>
             <div class="media-content is-grouped-centered">
-                <div class="label">Valgte Seter:
+                <div class="label">Setenummer:
                     <div class="content">
                         <div v-for="seat of seats" v-bind:key="seat">{{seat.number}}
-                            <span v-if="seats.length" text="."></span>
                         </div>
                     </div>
                 </div>
@@ -19,7 +18,7 @@
                 </div>
 
             </div>
-            <div class="column card-footer">
+            <div class="card-footer column">
                 <div class="is-italic">Billettene til BetaLAN er forhåndskjøpte
                     og du som kunden vil ikke motta tjenesten før BetaLAN er
                     gjennomført i sin helhet. Ved å klikke 'Aksepter betingelser'
@@ -27,7 +26,16 @@
                     vår.
                 </div>
             </div>
-            <div class="card-footer-item is-three-quarters"></div>
+            <div class="card-footer-item is-center column is-half">
+                <p class="label">Choose phone number for payment</p>
+                <input
+                    class="input is-primary"
+                    id="phoneNumber"
+                    type="text"
+                    v-model="ticket.phoneNumber"
+                    required
+                >
+            </div>
             <div class="card-footer-item">
                 <img src="@/assets/img/Betal med Vipps - 250px@3x.png" class="button is-paddingless is-medium" @click="initiateVippsPayment"/>
             </div>
@@ -50,53 +58,54 @@ export default {
       seats: {},
     };
   },
-  created() {
-    const { id } = this.$route.params;
-    const self = this;
-    const token = localStorage.getItem('token');
-    const config = {
-      headers: { Authorization: `bearer ${token}` },
-    };
+    created() {
+        const id = this.$route.params.id;
+        const self = this;
+        var token = localStorage.getItem("token");
+        var config = {
+            headers: { Authorization: "bearer " + token }
+        };
 
-    axios
-      .get(`/api/ticket/get/${id}`, config)
-      .then((response) => {
-        console.log(response);
-        self.ticket = response.data;
-        self.seats = response.data.eventSeats;
-      })
-      .catch((error) => {
-        console.log(error.response);
-      });
-  },
-  methods: {
-    initiateVippsPayment() {
-      const { id } = this.$route.params;
-      const self = this;
-      const token = localStorage.getItem('token');
-      const config = {
-        headers: { Authorization: `bearer ${token}` },
-      };
-
-      const bodyParam = {
-        id,
-        mobileNumber: this.ticket.phoneNumber,
-      };
-
-      console.log(bodyParam);
-      axios
-        .post('/api/ticket/initiatepayment', bodyParam, config)
-        .then((response) => {
-          console.log(response.data);
-          window.location = response.data;
-        })
-        .catch((error) => {
-          console.log(error.response);
-        });
+        axios
+            .get("/api/ticket/get/"+id,config)
+            .then(function (response) {
+                console.log(response);
+                self.ticket = response.data;
+                self.seats = response.data.eventSeats;
+            })
+            .catch(function (error) {
+                console.log(error.response);
+            });
     },
-  },
+    methods:{
+        initiateVippsPayment(){
+            const id = this.$route.params.id;
+            const self = this;
+            var token = localStorage.getItem("token");
+            var config = {
+                headers: { Authorization: "bearer " + token }
+            };
+
+            var bodyParam = {
+                id:id,
+                mobileNumber: this.ticket.phoneNumber,
+            };
+
+            console.log(bodyParam);
+            axios
+                .post("/api/ticket/initiatepayment",bodyParam,config)
+                .then(function (response) {
+                    console.log(response.data);
+                    window.location = response.data;
+                })
+                .catch(function (error) {
+                    console.log(error.response);
+                });
+        }
+    }
 };
 </script>
+
 
 <style scoped>
 
